3c12a0bff8dde49564932df67dae0542aced1f23
Titus Mbanu <mbanuobinna@gmail.com>
2023-10-13 03:48:43 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
  * main - prints numbers 0 to 9.
@@ -11,7 +11,7 @@ int main(void)
 
 	for (i = 48; i < 58; i++)
 	{
-	putchar(i);
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
