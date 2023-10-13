cc65f404654569717eb306d4dc2525d987b8bfa4
Titus Mbanu <mbanuobinna@gmail.com>
2023-10-13 03:42:08 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - prints numbers 0 to 9.
+ * 
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
