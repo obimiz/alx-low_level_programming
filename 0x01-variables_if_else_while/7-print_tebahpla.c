c879f1adee1329a1cbb490ee7fd53e95b54f03a2
Titus Mbanu <mbanuobinna@gmail.com>
2023-10-14 01:10:49 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-
 /**
  * main - prints the lower case alphabet in reverse.
  *
  * Return: Always 0 (success)
  */
 int main(void); {
-	char ch;
+	char c;
 
-	for (ch = 'z'; ch >= 'a'; ch--)
+	ch = 'z';
+	while
+		(c >= 'a')
 	{
-		putchar(ch);
+		putchar(c)
+		c--;
 	}
 	putchar('\n');
 	return (0);
