9c14cc05baf82fee0e91ea14326adcb1e1d359e0
Titus Mbanu <mbanuobinna@gmail.com>
2023-10-17 04:01:35 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes an int as a parameter and updates it to 98
+ * @n: the value to be updated to 98
+ * Return: always 0
+ */
+void reset_to_98 (int *n)
+{
+	*n = 98;
+}
