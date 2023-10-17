bbe049765957da3af51065390f181d8bd8a5098f
Titus Mbanu <mbanuobinna@gmail.com>
2023-10-17 10:22:11 -0700

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * -put - function that prints a string
+ * _put - function that prints a string
  *  @str:- pointer to the string
  *  Return: always return 0
  */
@@ -11,7 +11,9 @@ void _puts(char *str)
 	int i = 0;
 
 	while (str[i])
+	{
 		_putchar(str[i]);
 	i++;
+	}
 	_putchar('\n');
 }
