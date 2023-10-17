ca7b5baa2792eeff0e53b0a1330b46f0abe3ce21
Titus Mbanu <mbanuobinna@gmail.com>
2023-10-17 09:10:59 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * string_length - a function that returns the lenght of a string
+ * @s: string to be checked
+ * Return: return the length of a string
+ */
+
+int _strlen(char *s)
+{ 
+  
+	int string_length = 0;
+
+	while (s[string_length])
+		string_length++;
+	return (string_length);
+}
