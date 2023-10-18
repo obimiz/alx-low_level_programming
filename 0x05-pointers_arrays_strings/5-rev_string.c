0617a4cc35829a2f1f9c1fb1bfa08e02a1926268
Titus Mbanu <mbanuobinna@gmail.com>
2023-10-17 17:25:38 -0700

    re_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,14 +7,13 @@
  */
 void rev_string(char *s)
 {
-        int length, z, half;
-        char temp;
+	int length, z, half;
+	char temp;
 
         for (length = 0; s[length] != '\0'; length++)
         ;
         z = 0;
         half = length / 2;
-
         while (half--)
         {
                 temp = s[length - z - 1];
