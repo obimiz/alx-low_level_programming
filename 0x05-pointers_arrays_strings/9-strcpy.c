531f880e12ec61801df24cb088f1f873578dc2c6
Titus Mbanu <mbanuobinna@gmail.com>
2023-10-18 04:53:20 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,7 +2,7 @@
 
 /**
  * *_strcpy - copies the string pointer to src
- * @desk: char type to string
+ * @dest: char type to string
  * @src: char type string
  * Description: copy the string pointed to by pointer `src` to
  * the buffer pointed to desk
@@ -15,8 +15,7 @@ char *_strcpy(char *dest, char *src)
 	do {
 		z++;
 			dest[z] = src[z];
-	}
-	while (src[z] != '\0');
+	}while (src[z] != '\0');
 
 	return (dest);
 }
