33f04fe78ec9fe7ff0354174ee07d90325fc9f4c
Titus Mbanu <mbanuobinna@gmail.com>
2023-10-24 09:05:13 -0700

    fleet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,25 +1,20 @@
-#include "main.h"
-
 /**
- * leet - encode into 1337speak
- * @n: Input value
- * Return: n value
+ * leet - encodes a string into leetspeek.
+ * @s: pointer to input string.
+ * Return: Returns pointer to leetspeek string.
  */
-char *leet(char *n)
+char *leet(char *s)
 {
 	int i, j;
-	char s1[] = "aAeEoOtTlL";
-	char s2[] = "4433007711";
+	char subs[] = "aAeEoOtTlL";
+	char le[] = "43071";
 
-	for (i = 0; n[1] != '\0'; i++)
+	i = 0;
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 10; j++)
-		{
-			if (n[i] == s1[j])
-			{
-				n[i] = s2[j];
-			}
-		}
+		for (j = 0; subs[j] != '\0'; j++)
+			if (s[i] == subs[j])
+				s[i] = le[j / 2];
 	}
-	return (n);
+	return (s);
 }
