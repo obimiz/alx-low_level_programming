18e1e3c629a14120cc1d2a682e4b7bd672475faa
Titus Mbanu <mbanuobinna@gmail.com>
2023-10-30 03:26:37 -0700

    more-numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,14 @@ void more_numbers(void)
 {
         int x, y;
 
-        for (x = 0; x < 10; x++)
+        for (x = 1; x < 10; x++)
 	{
 		for (y = 0; y <= 14; y++)
 	{
 		if (y > 9)
-	{
-		_putchar((y / 10) + '0');
-	}
+	
+			_putchar((y / 10) + '0');
+	
 		_putchar((y % 10) + '0');
 	}
 		_putchar('\n');
