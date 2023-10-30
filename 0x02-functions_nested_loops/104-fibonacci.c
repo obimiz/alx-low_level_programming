272a1facde5a58feedd0371dea5a47f11d6c65f1
Titus Mbanu <mbanuobinna@gmail.com>
2023-10-30 02:34:10 -0700

    My teacher gave me an A+

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/**
+  * main - print the first 98 fibonacci numbers.
+  * Return: Nothing.
+  */
+int main(void)
+{
+	int count;
+	unsigned long i, j, k;
+	unsigned long m, n, o, carry;
+
+	count = 0;
+	i = 0;
+	j = 1;
+	for (count = 1; count <= 91; count++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%lu, ", k);
+	}
+	m = i % 1000;
+	i = i / 1000;
+	n = j % 1000;
+	j = j / 1000;
+	while (count <= 98)
+	{
+		carry = (m + n) / 1000;
+		o = (m + n) - carry * 1000;
+		k = (i + j) + carry;
+		m = n;
+		n = o;
+		i = j;
+		j = k;
+		if (o >= 100)
+			printf("%lu%lu", k, o);
+		else
+			printf("%lu0%lu", k, o);
+		if (count != 98)
+			printf(", ");
+		count++;
+	}
+	putchar('\n');
+	return (0);
+}i
