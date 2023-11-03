b4f05ce29f0bfbb7326603862c63603a3c39b396
Titus Mbanu <mbanuobinna@gmail.com>
2023-11-03 00:17:38 -0700

    print name of a program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print name of a function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+		return (0);
+}
