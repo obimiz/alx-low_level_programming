2fe0009913a1a0d1ba66f38662738606107ebc69
Titus Mbanu <mbanuobinna@gmail.com>
2023-11-10 08:08:13 -0800

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - This  returns the length of a string.
+ * @s: string
+ * Return: the length of  string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
