09397a0603f5d8b47395459a76fc8832c93bdf0b
Titus Mbanu <mbanuobinna@gmail.com>
2023-11-21 05:28:16 -0800

    listint_len

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * listint_len - returns the number of elements in a 
+ * listint_len - returns the number of elements in a
  linked listint_t list
  * @h: a pointer to the head of the listint_t list
  *
