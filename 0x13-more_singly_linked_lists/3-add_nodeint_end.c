4cfe0ae4a345c195eb4cac2425298cbd5b049f68
Titus Mbanu <mbanuobinna@gmail.com>
2023-11-21 06:39:02 -0800

    refactor

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * add_nodeint_end - adds a new node at the end of
@@ -16,6 +17,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new, *last;
 
 	new = malloc(sizeof(listint_t));
+	printf("%ld", sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
@@ -28,7 +30,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	{
 		last = *head;
 		while (last->next != NULL)
-		last = last->next;
+			last = last->next;
 		last->next = new;
 	}
 	return (*head);
